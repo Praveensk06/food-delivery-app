e156216b685311253bf551e33dab7ec5a25f0c71
Praveensk06 <113981543+Praveensk06@users.noreply.github.com>
2023-03-16 08:58:51 +0530

    completed  project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import BannerName from "./components/BannerName";
+import SubMenuContainer from "./components/styles/SubMenuContainer";
+import MenuCard from "./components/MenuCard";
+import "./components/styles/itemcard.css";
+import "./components/styles/menuCard.css";
+import "./components/styles/dishContainer.css";
+import DebitCard from "./components/DebitCard";
+import ItemCard from "./components/ItemCard";
+import { MenuItems, Items } from "./components/data";
+import CartItems from "./components/CartItems";
+import { useStateValue } from "./components/styles/StateProvider";
 
-function App() {
+const App = () => {
+  // main dish state variable
+  const [mainData, setMainData] = useState(
+    Items.filter((element) => element.itemId == "buger01")
+  );
+
+  const [{ cart }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    const menuLi = document.querySelectorAll("#menu li");
+
+    // toggle the active class for menu card when they clicked
+    function setMenuActive() {
+      menuLi.forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
+    }
+
+    menuLi.forEach((n) => n.addEventListener("click", setMenuActive));
+
+    // menu card active toggle
+    const menuCards = document
+      .querySelector(".row-container")
+      .querySelectorAll(".row-menu-card");
+
+    function setMenuActive() {
+      menuCards.forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
+    }
+    menuCards.forEach((n) => n.addEventListener("click", setMenuActive));
+  }, [mainData, cart]);
+
+  // set main dish items on filter passed item id
+
+  const setData = (itemId) => {
+    setMainData(Items.filter((element) => element.itemId === itemId));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* header section */}
+      <Header />
+      {/* main container */}
+      <main>
+        <div className="main-container">
+          {/* banner */}
+          <div className="banner">
+            <BannerName name={"praveen"} discount={"20"} link="#" />
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdelivery.png?alt=media&token=69b9823d-96df-452a-bd4e-14d27a4cc33"
+              alt=""
+              className="delivery-pic"
+            />
+          </div>
+          {/* dish container */}
+          <div className="dish-container">
+            <div className="menu-card">
+              <SubMenuContainer />
+            </div>
+            {/* categories row  */}
+            <div className="row-container">
+              {MenuItems &&
+                MenuItems.map((data) => (
+                  <div key={data.id} onClick={() => setData(data.itemId)}>
+                    <MenuCard
+                      name={data.name}
+                      imgSrc={data.imgSrc}
+                      isActive={data.itemId === "buger01" ? true : false}
+                    />
+                  </div>
+                ))}
+              ;
+            </div>
+
+            {/* dish items container */}
+            <div className="dish-item-container">
+              {mainData &&
+                mainData.map((data) => (
+                  <ItemCard
+                    key={data.id}
+                    itemId={data.id}
+                    imgSrc={data.imgSrc}
+                    name={data.name}
+                    ratings={data.ratings}
+                    price={data.price}
+                  />
+                ))}
+              ;
+            </div>
+          </div>
+        </div>
+        {/* right menu */}
+        <div className="right-menu">
+          <div className="debit-card-container">
+            <div className="debit-card">
+              <DebitCard />
+            </div>
+          </div>
+
+          {/* cart checkout */}
+          {/* if cart state is false it will render an empty div otherwise it will render a sub menu container */}
+          {!cart ? (
+            <div></div>
+          ) : (
+            <div className="cart-checkout-container">
+              <div className="cart-container">
+                <SubMenuContainer name={"cartItems"} />
+
+                <div className="cart-items">
+                  {cart &&
+                    cart.map((data) => (
+                      <CartItems
+                        name={data.name}
+                        imgSrc={data.imgSrc}
+                        price={data.price}
+                        key={data.id}
+                        itemId={data.id}
+                      />
+                    ))}
+                </div>
+              </div>
+              <div className="total-section">
+                <h3>total</h3>
+                <p>
+                  <span>$</span>45.0
+                </p>
+              </div>
+              <button className="check-out">checkout</button>
+            </div>
+          )}
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/CartItems.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+import { actionType } from "./reducer";
+import { useStateValue } from "./styles/StateProvider";
+
+// initialize an empty array for the cart items
+let cartItems = [];
+
+const CartItems = ({ imgSrc, name, price, itemId }) => {
+  // Declare a state variable named qty and set its initial value to 1
+  const [qty, setQty] = useState(1);
+
+  // Get the cart state and dispatch function from the StateProvider using the useStateValue hook
+  const [{ cart }, dispatch] = useStateValue();
+
+  // product od qty * price
+  const [itemPrice, setItemPrice] = useState(parseInt(qty) * parseFloat(price));
+
+  // a side effect whenever qty changes update cart items
+  useEffect(() => {
+    cartItems = cart;
+    setItemPrice(parseInt(qty) * parseFloat(price));
+  }, [qty]);
+
+  // updateQuantity that takes an action (add/remove) and an item ID as arguments
+  const updateQuantity = (action, id) => {
+    if (action == "add") {
+      setQty(qty + 1);
+    } else {
+      if (qty == 1) cartItems.pop(id);
+      dispatch({
+        type: actionType.SET_CART,
+        cart: cartItems,
+      });
+      setQty(qty - 1);
+    }
+  };
+  return (
+    <div className="cart-item">
+      <div className="img-box">
+        <img src={imgSrc} alt="" />
+      </div>
+      <div className="item-section">
+        <h2 className="item-name">{name}</h2>
+        <div className="item-quantity">
+          <span>x{qty}</span>
+          <div className="quantity">
+            <AiOutlineMinus
+              className="item-remove"
+              onClick={() => updateQuantity("remove", itemId)}
+            />
+
+            <AiOutlinePlus
+              className="item-add"
+              onClick={() => updateQuantity("add", itemId)}
+            />
+          </div>
+        </div>
+      </div>
+      <p className="item-price">
+        <span className="dolar-sign">$</span>
+        <span className="item-price-value">{itemPrice}</span>
+      </p>
+    </div>
+  );
+};
+
+export default CartItems;
